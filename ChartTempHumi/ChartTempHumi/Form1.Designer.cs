a9cab6a1e3a093ce3a3733edebc9852d63fe5778
heartcom <70210404+heartcom@users.noreply.github.com>
2025-05-12 13:11:04 +0900

    Add files via upload

---
+++ ChartTempHumi/ChartTempHumi/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ChartTempHumi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTemp = new System.Windows.Forms.TextBox();
+            this.txtHumi = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Navy;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(801, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림체", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(193, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "온습도 모니터링 프로그램";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConnect.Font = new System.Drawing.Font("굴림", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnConnect.Location = new System.Drawing.Point(708, 109);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "연결";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("굴림", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(579, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 139);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(776, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(97, 466);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "온도";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(458, 466);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "습도";
+            // 
+            // txtTemp
+            // 
+            this.txtTemp.Font = new System.Drawing.Font("굴림", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtTemp.Location = new System.Drawing.Point(154, 464);
+            this.txtTemp.Name = "txtTemp";
+            this.txtTemp.Size = new System.Drawing.Size(124, 29);
+            this.txtTemp.TabIndex = 6;
+            this.txtTemp.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtHumi
+            // 
+            this.txtHumi.Font = new System.Drawing.Font("굴림", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtHumi.Location = new System.Drawing.Point(515, 464);
+            this.txtHumi.Name = "txtHumi";
+            this.txtHumi.Size = new System.Drawing.Size(124, 29);
+            this.txtHumi.TabIndex = 7;
+            this.txtHumi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 543);
+            this.Controls.Add(this.txtHumi);
+            this.Controls.Add(this.txtTemp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTemp;
+        private System.Windows.Forms.TextBox txtHumi;
+    }
+}
+
---
+++ ChartTempHumi/ChartTempHumi/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ChartTempHumi
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            SetupChart();
+            LoadSerialPorts();
+        }
+        private void SetupChart()
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+
+            ChartArea area = new ChartArea("MainArea");
+            chart1.ChartAreas.Add(area);
+
+            // 온도 시리즈
+            Series tempSeries = new Series("온도");
+            tempSeries.ChartType = SeriesChartType.Line;
+            tempSeries.Color = Color.Red;
+            tempSeries.MarkerStyle = MarkerStyle.Circle;  // 도형: 원
+            tempSeries.MarkerSize = 8;                    // 크기
+            tempSeries.MarkerColor = Color.Red;           // 마커 색상
+            tempSeries.ChartArea = "MainArea";
+            chart1.Series.Add(tempSeries);
+
+            // 습도 시리즈
+            Series humidSeries = new Series("습도");
+            humidSeries.ChartType = SeriesChartType.Line;
+            humidSeries.Color = Color.Blue;
+            humidSeries.MarkerStyle = MarkerStyle.Diamond;  // 도형: 다이아몬드
+            humidSeries.MarkerSize = 8;                     // 크기
+            humidSeries.MarkerColor = Color.Blue;           // 마커 색상
+            humidSeries.ChartArea = "MainArea";
+            chart1.Series.Add(humidSeries);
+        }
+        private void LoadSerialPorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            Array.Sort(ports);
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(ports);
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+            else
+            {
+                comboBox1.Text = "None";
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String selectedPort = comboBox1.SelectedItem.ToString();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                string selectedPort = comboBox1.SelectedItem.ToString();
+                if (string.IsNullOrEmpty(selectedPort) || selectedPort == "None")
+                {
+                    MessageBox.Show("Please select a valid Port.");
+                    return;
+                }
+                try
+                {
+                    serialPort = new SerialPort(selectedPort)
+                    {
+                        BaudRate = 115200,
+                        DataBits = 8,
+                        Parity = Parity.None,
+                        StopBits = StopBits.One,
+                    };
+                    serialPort.DataReceived += SerialPort_DataReceived;   //Event Handler
+                    serialPort.Open();
+                    MessageBox.Show("Serial Port Connected");
+                    btnConnect.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Fail to Connect : {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid Port.");
+            }
+        }
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string line = serialPort.ReadLine().Trim(); // 예: "23.5,45.2"
+                string[] parts = line.Split(',');
+                if (parts.Length == 2 &&
+                         float.TryParse(parts[0], out float temp) &&
+                         float.TryParse(parts[1], out float humid))
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        txtTemp.Text = temp.ToString("0.0");
+                        txtHumi.Text = humid.ToString("0.0");
+
+                        // Chart에 데이터 추가
+                        chart1.Series["온도"].Points.AddY(temp);
+                        chart1.Series["습도"].Points.AddY(humid);
+
+                        // 포인트 수 제한 (최근 50개)
+                        if (chart1.Series["온도"].Points.Count > 50)
+                        {
+                          chart1.Series["온도"].Points.RemoveAt(0);
+                          chart1.Series["습도"].Points.RemoveAt(0);
+                        }
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                // 에러 무시하거나 로깅
+            }
+        }
+    }
+}
+
